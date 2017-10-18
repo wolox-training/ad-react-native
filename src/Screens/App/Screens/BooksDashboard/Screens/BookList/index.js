09cf97fe904381d82ec21ef08946ec1dd10e02a9
aducret <argentino.ducret@wolox.com.ar>
2017-10-18 15:55:38 -0300

    Moved book list login into children

--- src/Screens/App/Screens/BooksDashboard/Screens/BookList/index.js
+++ src/Screens/App/Screens/BooksDashboard/Screens/BookList/index.js
@@ -1,31 +1,12 @@
-import React from 'react';
 import { connect } from 'react-redux';
 
-import Book from './components/Book';
 import BookListComponent from './components/BookListComponent';
-
-const search = (text, filter) => (
-  text.toLowerCase().indexOf(filter.toLowerCase()) > -1
-);
-
-const check = (filterType, filter) => (
-  (book) => {
-    if (filterType && filter) {
-      return search(book[filterType], filter);
-    }
-    return true;
-  }
-);
-
-const generateBookList = (books, filterType, filter) =>
-  books
-    .filter(check(filterType, filter))
-    .map(book =>
-      <Book key={book.id} title={book.title} author={book.author} src={book.image_url} />);
 
 const mapStateToProps = state => (
   {
-    books: generateBookList(state.books, state.filterType, state.filter)
+    books: state.books,
+    filterType: state.filterType,
+    filter: state.filter
   }
 );
 
--- src/screens/App/screens/BooksDashboard/screens/BookList/components/BookListComponent/index.js
+++ src/screens/App/screens/BooksDashboard/screens/BookList/components/BookListComponent/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Book from '../Book';
 import './style.css';
 
-const BookListComponent = ({ books }) => (
+const search = (text, filter) => (
+  text.toLowerCase().indexOf(filter.toLowerCase()) > -1
+);
+
+const check = (filterType, filter) => (
+  (book) => {
+    if (filterType && filter) {
+      return search(book[filterType], filter);
+    }
+    return true;
+  }
+);
+
+const generateBookList = (books, filterType, filter) =>
+  books
+    .filter(check(filterType, filter))
+    .map(book =>
+      <Book key={book.id} title={book.title} author={book.author} src={book.image_url} />);
+
+const BookListComponent = ({ books, filterType, filter }) => (
   <div className="book-list-container">
-    {books}
+    {generateBookList(books, filterType, filter)}
   </div>
 );
 
 BookListComponent.propTypes = {
-  books: PropTypes.arrayOf(PropTypes.object)
+  books: PropTypes.arrayOf(PropTypes.object),
+  filterType: PropTypes.string,
+  filter: PropTypes.string
 };
 
 export default BookListComponent;
